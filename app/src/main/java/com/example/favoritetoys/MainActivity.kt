f4d18bbe7cdfcaec26bb61522b548996bcd551db
MatthewLC <mateusluiztj@hotmail.com>
2021-12-30 16:25:42 -0300

    Lesson 1 Exercise 2

--- app/src/main/java/com/example/favoritetoys/MainActivity.kt
+++ app/src/main/java/com/example/favoritetoys/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.favoritetoys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mToysListTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mToysListTextView = findViewById(R.id.tv_toy_names)
+        val toyNames = ToyBox.getToyNames()
+
+        toyNames.forEach { toyName ->
+            mToysListTextView.append("$toyName \n\n\n")
+        }
     }
 }
