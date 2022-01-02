0ff9a4678a4d7f24f39b4de9adc158cc7a06e171
MatthewLC <mateusluiztj@hotmail.com>
2022-01-01 23:15:04 -0300

    Lesson 02 Exercise 03

--- app/src/main/java/com/example/favoritetoys/MainActivity.kt
+++ app/src/main/java/com/example/favoritetoys/MainActivity.kt
@@ -2,8 +2,13 @@ package com.example.favoritetoys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.example.favoritetoys.R.id.action_search
+import com.example.favoritetoys.utilities.NetworkUtils
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,4 +23,27 @@ class MainActivity : AppCompatActivity() {
         mUrlDisplayTextView = findViewById(R.id.tv_url_display)
         mSearchResultsTextView = findViewById(R.id.tv_github_search_results_json)
     }
+
+    fun makeGithubSearchQuery(){
+        val query = mSearchBoxEditText.text.toString()
+        val url = NetworkUtils.buildUrl(query)
+        mUrlDisplayTextView.text = url.toString()
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if(id == action_search) {
+           makeGithubSearchQuery()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
