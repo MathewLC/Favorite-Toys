584a8a86b00c5e6a2898a3de13d2b8fbd2f2e69b
MatthewLC <mateusluiztj@hotmail.com>
2022-01-08 14:31:17 -0300

    Lesson 2 Exercise 5

--- app/src/main/java/com/example/favoritetoys/MainActivity.kt
+++ app/src/main/java/com/example/favoritetoys/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.favoritetoys
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,6 +12,7 @@ import android.widget.Toast
 import com.example.favoritetoys.R.id.action_search
 import com.example.favoritetoys.utilities.NetworkUtils
 import java.io.IOException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,36 +28,51 @@ class MainActivity : AppCompatActivity() {
         mSearchResultsTextView = findViewById(R.id.tv_github_search_results_json)
     }
 
-    private fun makeGithubSearchQuery(){
+    /**
+     * This method retrieves the search text from the EditText, constructs the URL
+     * (using {@link NetworkUtils}) for the github repository you'd like to find, displays
+     * that URL in a TextView, and finally fires off an AsyncTask to perform the GET request using
+     * our (not yet created) {@link GithubQueryTask}
+     */
+    private fun makeGithubSearchQuery() {
         val query = mSearchBoxEditText.text.toString()
         val url = NetworkUtils.buildUrl(query)
         mUrlDisplayTextView.text = url.toString()
 
+        GithubQueryTask().execute(url)
 
-        val response =
+    }
+
+    inner class GithubQueryTask : AsyncTask<URL, Void, String>() {
+
+        override fun doInBackground(vararg params: URL?): String? {
+            val searchUrl = params[0]
+            var gitHubSearchResults: String? = null
             try {
-                url?.let {
-                    NetworkUtils.getResponseFromHttpUrl(url)
-                }
-            } catch (ex: IOException){
-                Log.d("MainActivity",ex.stackTrace.toString())
-                null
+                gitHubSearchResults = searchUrl?.let { NetworkUtils.getResponseFromHttpUrl(it) }
+            } catch (ex: IOException) {
+                ex.printStackTrace()
             }
-
-        response?.let {
-            mSearchResultsTextView.text = response
+            return gitHubSearchResults
         }
+
+        override fun onPostExecute(result: String?) {
+            result?.let {
+                mSearchResultsTextView.text = if(result.isNotEmpty()) result else ""
+            }
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main,menu)
+        menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
-        if(id == action_search) {
-           makeGithubSearchQuery()
+        if (id == action_search) {
+            makeGithubSearchQuery()
             return true
         }
         return super.onOptionsItemSelected(item)
